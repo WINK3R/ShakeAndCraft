bb1a0eacbe5b54fd3a4a6d6970ab9e84e2500545
Lucas Delanier <delanierlucas@gmail.com>
2023-03-02 23:59:42 +0100

    initial commit

---
+++ app/src/main/java/com/example/shakecraft/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.shakecraft
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var bottomNav : BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        loadFragment(HomeFragment())
+        bottomNav = findViewById(R.id.bottomNav) as BottomNavigationView
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.inventory -> {
+                    loadFragment(InventoryFragment())
+                    true
+                }
+                R.id.plus -> {
+                    loadFragment(PlusFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,fragment)
+        transaction.commit()
+    }
+
+
+}
