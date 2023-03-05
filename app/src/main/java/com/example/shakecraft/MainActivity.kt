7861a8fe1d8ea5d4aea236e101d913f5a70f7b27
Lucas Delanier <delanierlucas@gmail.com>
2023-03-05 18:27:36 +0100

    new pages

--- app/src/main/java/com/example/shakecraft/MainActivity.kt
+++ app/src/main/java/com/example/shakecraft/MainActivity.kt
@@ -1,15 +1,41 @@
 package com.example.shakecraft
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-    lateinit var bottomNav : BottomNavigationView
 
+
+
+
+    // Function to hide NavigationBar
+    @RequiresApi(Build.VERSION_CODES.R)
+    private fun hideSystemUI() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window,
+            window.decorView.findViewById(android.R.id.content)).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+
+            // When the screen is swiped up at the bottom
+            // of the application, the navigationBar shall
+            // appear for some time
+            controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+    }
+
+    lateinit var bottomNav : BottomNavigationView
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+        hideSystemUI()
         setContentView(R.layout.activity_main)
         loadFragment(HomeFragment())
         bottomNav = findViewById(R.id.bottomNav) as BottomNavigationView
@@ -24,7 +50,7 @@ class MainActivity : AppCompatActivity() {
                     true
                 }
                 R.id.plus -> {
-                    loadFragment(PlusFragment())
+                    loadFragment(CollectFragment())
                     true
                 }
                 else -> false
