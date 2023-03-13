dcc94a9845aeb3e6d2097a31147b07637837dfdd
Lucas Delanier <delanierlucas@gmail.com>
2023-03-13 23:11:55 +0100

    add plus fragment and wiki button redirect to codefirst repository :tada:

--- ShakeAndCraft/app/src/main/java/com/example/shakecraft/PlusFragment.kt
+++ ShakeAndCraft/app/src/main/java/com/example/shakecraft/PlusFragment.kt
@@ -1,13 +1,16 @@
 package com.example.shakecraft
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 
 class PlusFragment : Fragment() {
-
+    private lateinit var buttonWiki : LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +22,23 @@ class PlusFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_plus, container, false)
+        val view = inflater.inflate(R.layout.fragment_plus, container, false)
+
+        // Initialize views
+        setUpRecyclerView(view)
+
+        return view
+    }
+
+    private fun setUpRecyclerView(view: View,) {
+        buttonWiki = view.findViewById(R.id.wikiButton)
+        buttonWiki.setOnClickListener{
+            val url = "https://codefirst.iut.uca.fr/git/lucas.delanier/ShakeAndCraft"
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(url)
+            intent.setPackage("com.android.chrome")
+            startActivity(intent)
+        }
     }
 
 
